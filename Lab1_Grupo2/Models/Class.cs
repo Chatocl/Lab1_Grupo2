3225f95f8a8dd6f09a0c6f920080b2facea6c13b
ROBERTO ALFREDO MOYA NOACK <ramoyan@correo.url.edu.gt>
2022-02-01 21:13:38 -0600

    pequeño camb

--- Lab1_Grupo2/Models/Class.cs
+++ Lab1_Grupo2/Models/Class.cs
@@ -13,7 +13,9 @@ namespace Lab1_Grupo2.Models
         public string Nombre_CL { get; set; }
         public string Apellido_CL { get; set; }
         public string Telefono_CL { get; set; }
-        public string Descripcion_CL { get; set; }
+        public string Descrion_CL { get; set; }
+        public string Poriacaso_CL { get; set; }
 
     }
 }
+//Comentario
