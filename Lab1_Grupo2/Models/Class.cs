7ad725ccd2a4ce7744f804a9a8be39f24ed03fc3
CARLOS ROBERTO CORONADO LAZO <crcoronado@correo.url.edu.gt>
2022-02-01 20:46:58 -0600

    Cree las clase clientes

---
+++ Lab1_Grupo2/Models/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_Grupo2.Models
+{
+    // Class Cliente Almacena Nombre, Apellido, Telefono, Descripcion
+    public class Clientes
+    {
+        [Required]
+        public string Nombre_CL { get; set; }
+        public string Apellido_CL { get; set; }
+        public string Telefono_CL { get; set; }
+        public string Descripcion_CL { get; set; }
+
+    }
+}
