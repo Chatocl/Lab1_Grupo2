b1ef8c3b049e460f43e18eeef10b2c3d616cab5b
mfcanesesr <mfcanesesr@gmail.com>
2022-02-05 22:55:31 -0600

    Edit Añadir cliente

--- Lab1_Grupo2/Models/Clientes.cs
+++ Lab1_Grupo2/Models/Clientes.cs
@@ -9,13 +9,15 @@ namespace Lab1_Grupo2.Models
     // Class Cliente Almacena Nombre, Apellido, Telefono, Descripcion
     public class Clientes
     {
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
         public int? Id {get;set;}
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
         public string Nombre { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
         public string Apellido { get; set; }
+        [Required(ErrorMessage = "El campo Teléfono es requerido.")]
         public int? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo Descripción es requerido.")]
         public string Descripcion { get; set; }
 
         
